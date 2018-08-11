93b2fe560ab31ce4458c6289bf3b289ca3ee6d5b
elisabethschiele <elisabethschiele@gmail.com>
2018-08-11 13:17:30 +0200

    regal added

--- src/pages/regal/regal.ts
+++ src/pages/regal/regal.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  selector: 'page-regal',
+  templateUrl: 'regal.html'
 })
-export class HomePage {
+export class RegalPage {
 
   constructor(public navCtrl: NavController) {
 
